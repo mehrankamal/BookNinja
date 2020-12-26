a70a6f5f6baec8dfb4867c8f3e769d337fad6986
Mehran Kamal <mkbalochhoth@gmail.com>
2020-12-26 21:31:42 +0500

    check_user middleware for authemtication added.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const cookerParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
 const pool = require("./db");
 const user_api_routes = require("./routes/api/user");
 const user_view_routes = require("./routes/views/user");
+const { check_user } = require("./middleware/jwtMiddleware");
+
 
 const app = express();
 
 //Middlewares
+
+
 app.use(cors());
 app.use(express.json());
-const bodyParser = require("body-parser");
 app.use(
     bodyParser.urlencoded({
         extended: true,
@@ -21,6 +25,8 @@ app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use("/public", express.static("public"));
 app.use(cookerParser());
+app.use(check_user);
+
 
 //API Routes
 app.use("/api/user", user_api_routes);
--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -7,6 +7,30 @@ const {require_auth} = require("../../middleware/jwtMiddleware");
 const router = express.Router();
 const saltRounds = 10;
 
+// @route   PUT api/user/:user_id/edit_bio
+// @desc    Edit bio of a user
+// @access  private (user can edit his own bio)
+
+router.put('/:user_id/edit_bio', require_auth, async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log("Error: " + err);
+    }
+});
+
+// @route   PUT api/user/:user_id/edit_name
+// @desc    Edit name of a user
+// @access  private (user can add shelf into his own account)
+
+router.put('/:user_id/edit_name', require_auth, async (req, res) => {
+    try {
+        
+    } catch (err) {
+        console.log("Error: " + err);
+    }
+});
+
 // @route   POST api/user/:user_id/add_shelf
 // @desc    Add a new shelf against an user given shelf name
 // @access  private (user can add shelf into his own account)
